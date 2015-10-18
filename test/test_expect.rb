36f20a7df089b633934746fc720aa1ac077fa305
Cyril Wack <contact@cyril.email>
2015-10-18 18:43:15 +0200

    fix JRuby builds

--- test/test_expect.rb
+++ test/test_expect.rb
@@ -7,7 +7,7 @@ begin
 
   Fix.describe app do
     it { expect_block {}.not_to Equal: -42 }
-    it { expect_block {}.not_to! Equal: -42 }
+    it { expect_block {}.not_to! Equal: -42 } if Process.respond_to?(:fork)
 
     it { MUST Equal: -42 }
     it { expect(app).to Equal: -42 }
@@ -16,7 +16,7 @@ begin
       it { MUST Equal: 42 }
       it { expect(:boom).to RaiseException: NoMethodError }
       it { expect(app).to Equal: 42 }
-      it { expect_block { app }.to! Equal: 42 }
+      it { expect_block { app }.to! Equal: 42 } if Process.respond_to?(:fork)
     end
   end
 rescue SystemExit => e
